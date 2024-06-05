91b005e69216f265d9cf88c98c3adb57f81c9fb2
bhanusharanya <sharanya2630@gmail.com>
2024-06-05 20:55:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+
+    let url = 'https://localhost:8080/';
+    const params = new URLSearchParams();
+
+    if (name) {
+        params.append('name', name);
+    }
+
+    if (year) {
+        params.append('year', year);
+    }
+
+    if (params.toString()) {
+        url += '?' + params.toString();
+    }
+
+    document.getElementById('url').textContent = url;
+});
